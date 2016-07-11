daccece352c2c6f0c0115acb6778c64a442334e1
firru <firru@users.noreply.github.com>
2016-07-11 17:19:31 +0200

    added in memory http/mock-service

--- app/dashbourd.component.ts
+++ app/dashbourd.component.ts
@@ -12,16 +12,16 @@ import { HeroService } from './hero.service';
 })
 export class DashboardComponent implements OnInit {
 	heroes: Hero[] = [];
-	
+
 	constructor(
 		private router: Router,
 		private heroService: HeroService) {
 		};
-	
+
 	ngOnInit() {
-		this.heroService.getHeroes().then(heroes => this.heroes = heroes.slice(1, 5));
+		this.heroService.getHeroes().then(heroes => this.heroes = heroes.slice(0, 5));
 	}
-	
+
 	gotoDetail(hero: Hero) {
 		let link = ['/detail', hero.id];
 		this.router.navigate(link);
--- app/hero.service.ts
+++ app/hero.service.ts
@@ -2,19 +2,42 @@
  * Created by firru on 27.06.16.
  */
 import { Injectable}  from "@angular/core";
+import { Http } from '@angular/http';
 import {HEROES} from "./mock-heroes";
+import {Hero} from "./hero";
+
+import 'rxjs/add/operator/toPromise';
 
 @Injectable()
 export class HeroService {
-    getHeroes() {
+
+    private heroesUrl = 'app/heroes';  // URL to web api
+
+    constructor(private http: Http) {
+    };
+
+    getHeroes(): Promise<Hero[]> {
+        return this.http.get(this.heroesUrl)
+                   .toPromise()
+                   .then(response => response.json().data)
+                   .catch(this.handleError);
+    };
+
+    private handleError(error: any) {
+        console.error('An error occurred', error);
+
+        return Promise.reject(error.message || error);
+    };
+
+    getMockHeroes() {
         return Promise.resolve(HEROES);
     };
 
     getHeroesSlowly() {
         return new Promise(resolve => setTimeout(() => resolve(HEROES), 2000));
     };
-	
-	getHero(id: number) {
+
+    getHero(id: number) {
 		return this.getHeroes().then(heroes => heroes.filter(hero => hero.id === id)[0]);
 	};
 }
